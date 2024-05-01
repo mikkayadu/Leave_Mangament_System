7a2d89fedf6bb32fab95ec594da8f805153941bb
Michael Adu-Gyamfi <michaeladugyamfi76@gmail.com>
2024-05-01 22:00:39 +0000

    Added project files

--- HR.LeaveManagement/HR.LeaveManagement.Application/Features/LeaveAllocation/Handlers/Queries/GetLeaveAllocationDetailRequestHandler.cs
+++ HR.LeaveManagement/HR.LeaveManagement.Application/Features/LeaveAllocation/Handlers/Queries/GetLeaveAllocationDetailRequestHandler.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.DTOs;
+using HR.LeaveManagement.Application.Features.LeaveAllocation.Requests.Queries;
+using HR.LeaveManagement.Application.Persistence.Contracts;
+using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +11,20 @@ using System.Threading.Tasks;
 
 namespace HR.LeaveManagement.Application.Features.LeaveAllocation.Handlers.Queries
 {
-     class GetLeaveAllocationDetailRequestHandler
+    public class GetLeaveAllocationDetailRequestHandler : IRequestHandler<GetLeaveAllocationDetailRequest, LeaveAllocationDto>
     {
+        private readonly ILeaveAllocationRepository _leaveAllocationRepository;
+        private readonly IMapper _mapper;
+
+        public GetLeaveAllocationDetailRequestHandler(ILeaveAllocationRepository leaveAllocationRepository, IMapper mapper)
+        {
+            _leaveAllocationRepository = leaveAllocationRepository;
+            _mapper = mapper;
+        }
+        public async Task<LeaveAllocationDto> Handle(GetLeaveAllocationDetailRequest request, CancellationToken cancellationToken)
+        {
+            var leaveAllocation = await _leaveAllocationRepository.Get(request.Id);
+            return _mapper.Map<LeaveAllocationDto>(leaveAllocation);
+        }
     }
 }
