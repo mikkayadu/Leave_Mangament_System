adc6704ce02e95a3d9b3812ce6f4a271bfacbb06
Michael Adu-Gyamfi <michaeladugyamfi76@gmail.com>
2024-05-10 22:59:37 +0000

    Added LeaveManagementDbContextFactory

---
+++ HR.LeaveManagement/HR.LeaveManagement.Persistence/LeaveManagementDbContextFactory.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace HR.LeaveManagement.Persistence
+{
+    public class LeaveManagementDbContextFactory : IDesignTimeDbContextFactory<LeaveManagementDbContext>
+    {
+        public LeaveManagementDbContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("C:\\Users\\Michael  Adu-Gyamfi\\Documents\\Leave_Mangament_System\\HR.LeaveManagement\\HR.LeaveManagement.API\\appsettings.json")
+                .Build();
+
+            var builder = new DbContextOptionsBuilder<LeaveManagementDbContext>();
+            var connectionString = configuration.GetConnectionString("LeaveManagementConnectionString");
+            builder.UseNpgsql(connectionString);
+
+            return new LeaveManagementDbContext(builder.Options);
+        }
+    }
+}
