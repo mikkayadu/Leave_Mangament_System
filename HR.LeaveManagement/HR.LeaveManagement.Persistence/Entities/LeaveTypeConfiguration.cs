faac76d5eeb9bcc16db76fd24eb592af7045fbe4
Michael Adu-Gyamfi <michaeladugyamfi76@gmail.com>
2024-05-14 18:47:54 +0000

    Added project files

---
+++ HR.LeaveManagement/HR.LeaveManagement.Persistence/Entities/LeaveTypeConfiguration.cs
@@ -0,0 +1,38 @@
+﻿using HR.LeaveManagement.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Persistence.Entities
+{
+    public class LeaveTypeConfiguration:IEntityTypeConfiguration<LeaveType>
+    {
+        public void Configure(EntityTypeBuilder<LeaveType> builder)
+        {
+            builder.HasData(
+new LeaveType
+{
+    Id = 1,
+    DefaultDays = 10,
+    Name = "Vacation",
+    CreatedBy = "Admin",    // Assuming default values are appropriate
+    LastModifiedBy = "Admin" // Include this to satisfy the model requirements
+},
+new LeaveType
+{
+    Id = 2,
+    DefaultDays = 12,
+    Name = "Sick",
+    CreatedBy = "Admin",
+    LastModifiedBy = "Admin"
+}
+);
+
+
+        }
+    }
+}
--- HR.LeaveManagement/HR.LeaveManagement.Persistence/Migrations/20240514182714_SeedingLeaveTypes.Designer.cs
+++ HR.LeaveManagement/HR.LeaveManagement.Persistence/Migrations/20240514182714_SeedingLeaveTypes.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using HR.LeaveManagement.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace HR.LeaveManagement.Persistence.Migrations
 {
     [DbContext(typeof(LeaveManagementDbContext))]
-    partial class LeaveManagementDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240514182714_SeedingLeaveTypes")]
+    partial class SeedingLeaveTypes
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
