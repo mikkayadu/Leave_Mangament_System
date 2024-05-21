de0d0be878ed7e40e255780f4c8129889eb4e0d1
Michael Adu-Gyamfi <michaeladugyamfi76@gmail.com>
2024-05-21 02:33:24 +0000

    Modified and updated the database

--- HR.LeaveManagement/HR.LeaveManagement.Persistence/Entities/LeaveRequestConfiguration.cs
+++ HR.LeaveManagement/HR.LeaveManagement.Persistence/Entities/LeaveRequestConfiguration.cs
@@ -11,6 +11,28 @@ namespace HR.LeaveManagement.Persistence.Configurations.Entities
     {
         public void Configure(EntityTypeBuilder<LeaveRequest> builder)
         {
+            builder.HasData(
+                new LeaveRequest
+                {
+                    Id = 1,
+                    StartDate = new DateTime(2024, 5, 25, 0, 0, 0, DateTimeKind.Utc),
+
+                    EndDate = new DateTime(2024, 5, 31, 0, 0, 0, DateTimeKind.Utc),
+                    LeaveTypeId = 1,
+                    RequestComments = "Very urgent",
+                    CreatedBy = "Admin",
+                    LastModifiedBy = "Admin",
+                    RequestingEmployeeId = "1",
+                    
+                    DateRequested = DateTime.UtcNow,
+                    //DateActioned = DateTime.Now.AddDays(1),
+                    //Approved = True
+
+                    Cancelled = false,
+
+
+                }) ;
+
 
 
         }
---
+++ HR.LeaveManagement/HR.LeaveManagement.Persistence/Migrations/20240521020734_SeedLeaveRequestData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HR.LeaveManagement.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedLeaveRequestData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "LeaveRequests",
+                columns: new[] { "Id", "Approved", "Cancelled", "CreatedBy", "DateActioned", "DateCreated", "DateRequested", "EndDate", "LastModifiedBy", "LastModifiedDate", "LeaveTypeId", "RequestComments", "RequestingEmployeeId", "StartDate" },
+                values: new object[] { 1, null, false, "Admin", null, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2024, 5, 21, 2, 7, 34, 680, DateTimeKind.Utc).AddTicks(197), new DateTime(2024, 5, 31, 0, 0, 0, 0, DateTimeKind.Utc), "Admin", new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 1, "Very urgent", "1", new DateTime(2024, 5, 25, 0, 0, 0, 0, DateTimeKind.Utc) });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "LeaveRequests",
+                keyColumn: "Id",
+                keyValue: 1);
+        }
+    }
+}
