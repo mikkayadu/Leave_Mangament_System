925df35517faabee2f951c4927d59b54130e64c5
Michael Adu-Gyamfi <michaeladugyamfi76@gmail.com>
2024-07-04 21:12:09 +0000

    Added IdentityServicesRegistration

--- HR.LeaveManagement/HR.LeaveManagement.Identity/IdentityServicesRegistration.cs
+++ HR.LeaveManagement/HR.LeaveManagement.Identity/IdentityServicesRegistration.cs
@@ -1,12 +1,24 @@
-﻿using System;
+﻿using HR.LeaveManagement.Application.Models.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace HR.LeaveManagement.Identity
 {
-    public class IdentityServicesRegistration
+    public static class IdentityServicesRegistration
     {
+        public  static IServiceCollection ConfigureIdentityServices(this IServiceCollection services, IConfiguration configuration)
+        {
+           services.Configure<JwtSettings>(configuration.GetSection("JwtSettings"));
+            services.AddDbContext<LeaveManagementIdentityDbContext>(options =>
+            options.UseNpgsql(configuration.GetConnectionString("LeaveManagementIdentityConnectionString"),
+            b=>b.Migration));
+        }
     }
 }
